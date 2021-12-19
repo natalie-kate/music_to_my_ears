7ff8795e6b94542db04d7f6d656f7dfed456ef7c
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-19 00:38:45 +0000

    Customise product admin view

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,29 @@
 from django.contrib import admin
 from .models import Genre, Vinyl, Image
 
-admin.site.register(Genre)
-admin.site.register(Vinyl)
-admin.site.register(Image)
+
+class VinylAdmin(admin.ModelAdmin):
+    """Change display in admin panel to see important information """
+    list_display = (
+        'artist',
+        'title',
+        'stock_quantity',
+        'genre'
+    )
+    ordering = ('artist',)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    """Change order in admin panel """
+    list_display = ('image_name', 'image', 'vinyl')
+    ordering = ('vinyl',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name',)
+    ordering = ('friendly_name',)
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Vinyl, VinylAdmin)
+admin.site.register(Image, ImageAdmin)
