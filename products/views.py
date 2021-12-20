217d866e5c1b5888f2e764c2040b7abbdfdeefdb
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-20 22:12:08 +0000

    Start open_shop view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Vinyl, Genre, Image
 
-# Create your views here.
+
+def open_shop(request):
+    """ A view to return the shopping page """
+    products = Vinyl.objects.all()
+    genres = Genre.objects.all()
+
+    images = Image.objects.all()
+    context = {
+        'products': products,
+        'genres': genres,
+        'images': images,
+    }
+    
+    return render(request, 'products/shop.html', context)
+
