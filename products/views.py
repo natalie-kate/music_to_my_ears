e1642458edb0da8153f83b11635097ad980e4c1e
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-21 01:06:39 +0000

    Add in search box functionality

--- products/views.py
+++ products/views.py
@@ -1,25 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Vinyl, Genre, Image
 
 
 def open_shop(request):
     """ A view to return the shopping page """
+
     products = Vinyl.objects.all()
     genres = Genre.objects.all()
-    
+    images = Image.objects.all()
     current_genres = []
+    search = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            search = request.GET['search']
+            searches = Q(title__icontains=search) | Q(
+                artist__icontains=search) | Q(track_list__icontains=search)
+            products = products.filter(searches)
+            if not products:
+                messages.error(
+                    request, (
+                        "Sorry your query didn't match any of our products"))
+                return redirect(reverse('shop'))
+
+        if not search:
+            messages.error(
+                request, "Oops you need to enter a search keyword first")
+            return redirect(reverse('shop'))
 
     for genre in genres:
-        filtered_products = Vinyl.objects.filter(genre= genre.pk)
+        filtered_products = Vinyl.objects.filter(genre=genre.pk)
         if filtered_products:
             current_genres.append(genre)
 
-    images = Image.objects.all()
     context = {
         'products': products,
         'genres': current_genres,
         'images': images,
+        'search': search,
     }
-    
-    return render(request, 'products/shop.html', context)
 
+    return render(request, 'products/shop.html', context)
