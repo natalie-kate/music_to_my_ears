e1e6e5b73c6a431e5d39e63a70af8cdce86df86c
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-27 21:15:22 +0000

    Add in def setUp and remove duplicated lines

--- products/test_views.py
+++ products/test_views.py
@@ -1,10 +1,28 @@
-""" Import TestCase to use to test our views """
+""" Import TestCase Vinyl and Genre to test our views """
 from django.test import TestCase
 from .models import Vinyl, Genre
 
 
 class TestViews(TestCase):
     """ Test product views """
+
+    def setUp(self):
+        """ Create test records to use """
+        self.genre = Genre.objects.create(
+                genre="test",
+                friendly_name="Test"
+            )
+        self.vinyl = Vinyl.objects.create(
+            title="Test",
+            artist="bla",
+            release_year="1950",
+            price="12.99",
+            description="testing testing",
+            track_list="test, test,test",
+            stock_quantity=5,
+            genre=self.genre
+        )
+
     def test_shop_view(self):
         """ Test shop.html rendered properly """
         response = self.client.get('/shop/')
@@ -13,23 +31,6 @@ class TestViews(TestCase):
 
     def test_product_view(self):
         """ Test product.html rendered properly """
-
-        test_genre = Genre.objects.create(
-            genre="test",
-            friendly_name="Test"
-        )
-        test_vinyl = Vinyl.objects.create(
-            title="Test",
-            artist="bla",
-            release_year="1950",
-            price="12.99",
-            description="testing testing",
-            track_list="test, test,test",
-            stock_quantity=5,
-            genre=test_genre
-        )
-        response = self.client.get(f'/{test_vinyl.id}/')
+        response = self.client.get(f'/{ self.vinyl.id }/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product.html')
-
-        
