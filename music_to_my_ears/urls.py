dc9423ccb57d2931f63cb84ea53f8aa987d99e41
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-27 22:16:11 +0000

    Add basket to url patterns

--- music_to_my_ears/urls.py
+++ music_to_my_ears/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('', include('products.urls')),
+    path('basket/', include('basket.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
