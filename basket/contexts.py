deca88d07b99cce940cdcb4f7c37dd540adb092b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-31 14:32:03 +0000

    Add in more variables, imports and for loop for items

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,12 +1,36 @@
-from products.models import Vinyl,Images
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Vinyl, Image
+
 
 def basket_contents(request):
-
+    image = Image.objects.all()
     basket_products = []
-    bag = request.session.get('basket', {})
+    total = 0
+    product_count = 0
+    basket = request.session.get('basket', {})
+    delivery = 4.95
+
+    for product_id, quantity in basket.items():
+        product = get_object_or_404(Vinyl, pk=product_id)
+        total += quantity * product.price
+        product_count += quantity
+        product_images = product.image_set.all()
+        basket_products.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+            'product_images': product_images,
+        })
+
+    grand_total = Decimal(total + delivery)
 
     context = {
-        'products' = basket_products
+        'basket_products': basket_products,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total
     }
 
     return context
