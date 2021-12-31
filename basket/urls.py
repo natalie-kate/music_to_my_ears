252aca7a8f809086b78c90cf8e3a0bf3d6524a41
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-12-31 15:18:21 +0000

    Remove spacing and changed basket to view_basket

--- basket/urls.py
+++ basket/urls.py
@@ -1,7 +1,8 @@
+""" Imports required basket app urls """
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('basket/', views.basket, name = 'basket'),
+    path('basket/', views.view_basket, name='basket'),
     path('add/<product_id>/', views.add_to_basket, name='add_to_basket'),
 ]
