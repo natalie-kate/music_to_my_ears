ec31f01d906530358521968cfad7818404b52dc3
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-06 21:31:05 +0000

    Add edit and delete urls, edit basket name

--- basket/urls.py
+++ basket/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('basket/', views.view_basket, name='basket'),
+    path('basket/', views.view_basket, name='view_basket'),
     path('add/<product_id>/', views.add_to_basket, name='add_to_basket'),
+    path('edit/<product_id>/', views.edit_quantity, name='edit_quantity'),
+    path('delete/<product_id>/', views.delete_item, name='delete_item'),
 ]
