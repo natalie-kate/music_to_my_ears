5850bd33a2bd4af2189217cdc9b672a0749841db
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-06 21:38:02 +0000

    Add docstrings, check in product view if product in basket

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+""" Imports required by products app """
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
@@ -46,14 +47,22 @@ def open_shop(request):
 
 
 def product(request, product_id):
+    """ Get product details """
+    quantity_in_basket = 0
     product_info = get_object_or_404(Vinyl, pk=product_id)
     images = Image.objects.filter(vinyl=product_id)
     tracklist = product_info.track_list.split(",")
+    basket = request.session.get('basket', {})
+    basket_product = product_id in list(basket.keys())
+    if basket_product:
+        quantity_in_basket = basket[product_id]
 
     context = {
         'product': product_info,
         'images': images,
-        'tracklist': tracklist
+        'tracklist': tracklist,
+        'basket_product': basket_product,
+        'quantity': quantity_in_basket,
     }
 
     return render(request, 'products/product.html', context)
