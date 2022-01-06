426308bf9576c5023c9fda908b064cd519d91a66
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-06 21:31:49 +0000

    Add edit and delete views, remove reverse import

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,5 @@
 """ Imports required for basket app views """
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from products.models import Vinyl
 
@@ -26,3 +26,27 @@ def add_to_basket(request, product_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def edit_quantity(request, product_id):
+    """ Edit quantity of basket product """
+    if request.method == 'POST':
+        product = get_object_or_404(Vinyl, pk=product_id)
+        quantity = request.POST.get('quantity')
+        basket = request.session.get('basket', {})
+        basket[product_id] = int(quantity)
+        messages.success(request, (
+            f"That's { product.title } quantity updated to { quantity }"))
+        request.session['basket'] = basket
+        return redirect('view_basket')
+
+
+def delete_item(request, product_id):
+    """ Delete product from basket """
+    product = get_object_or_404(Vinyl, pk=product_id)
+    basket = request.session.get('basket', {})
+    basket.pop(product_id)
+    messages.success(request, (
+            f"That's { product.title } removed from your basket."))
+    request.session['basket'] = basket
+    return redirect('view_basket')
