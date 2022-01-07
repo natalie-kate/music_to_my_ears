a28263bc6d41738708ba46bce9ca03bb84b5a085
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-07 23:49:08 +0000

    Create order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,34 @@
+""" Imports required for checkout app models """
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django_countries.fields import CountryField
+from products.models import Vinyl
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    surname = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=50, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    basket = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """ Generate an order number using UUID """
+        order_number = uuid.uuid4().hex.upper()
+        return order_number
