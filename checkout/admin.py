7088d92efba648cd8f2b6d713b039cab755e0e7d
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-08 01:32:47 +0000

    Created admin classes for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,35 @@
+""" Imports required for admin in checkout app """
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """ Outlining what we want to be able to edit in admin console for
+    the line items of an order """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """ Outlining ordering of information and what we want to be able
+    to edit in admin console for orders """
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'order_date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'basket',
+                       'stripe_pid')
+
+    fields = ('order_date', 'order_number', 'first_name', 'surname',
+              'email', 'phone_number', 'street_address1', 'street_address2',
+              'town_or_city', 'county', 'country',  'postcode', 'order_total',
+              'delivery_cost', 'grand_total', 'basket', 'stripe_pid')
+
+    list_display = ('order_number', 'order_date', 'surname',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
