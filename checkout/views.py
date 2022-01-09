b443388ebe0c21c5139aef13a747d174e8a33685
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-09 00:58:46 +0000

    Start chekout view in checkout app views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .forms import OrderForm, DeliveryForm
 
-# Create your views here.
+
+def checkout(request):
+    basket = request.session.get('basket')
+    if not basket:
+        messages.error(request, "You need to add something to checkout!")
+        return redirect(reverse('shop'))
+    order_form = OrderForm()
+    delivery_form = DeliveryForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'delivery_form': delivery_form
+    }
+    return render(request, template, context)
