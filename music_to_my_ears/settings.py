813312f4d710fe9724be82a527b01b017cf24aa4
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-09 01:00:23 +0000

    Add cripsy forms information to settings.py

--- music_to_my_ears/settings.py
+++ music_to_my_ears/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'music_to_my_ears.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
