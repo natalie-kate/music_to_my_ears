a1f9023cf516c865ba73b3f40773c4d2f0f9b0b9
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-13 00:19:07 +0000

    Add form submit event listener

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -17,12 +17,8 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var cardNumber = elements.create('cardNumber', {style: style});
-cardNumber.mount('#card-number');
-var cardExpiry = elements.create('cardExpiry', {style: style});
-cardExpiry.mount('#card-expiry');
-var cardCvc = elements.create('cardCvc', {style: style});
-cardCvc.mount('#card-cvc');
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
 
 card.addEventListener('change', function(event) {
     var errorDiv = document.getElementById('card-errors');
@@ -37,3 +33,33 @@ card.addEventListener('change', function(event) {
         errorDiv.textContent = '';
     }
 })
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
