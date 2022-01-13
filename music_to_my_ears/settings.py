3fa979704b8c04d195a48afebb1855da6ee142cc
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-13 00:21:07 +0000

    Add stripe variables

--- music_to_my_ears/settings.py
+++ music_to_my_ears/settings.py
@@ -174,3 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
