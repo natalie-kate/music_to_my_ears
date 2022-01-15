c444d0d9290c16c536f6992d8212baa2e6b20160
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-15 21:37:04 +0000

    Add to check for quantity before adding to basket_products

--- basket/contexts.py
+++ basket/contexts.py
@@ -2,6 +2,7 @@
 from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from products.models import Vinyl
+from django.contrib import messages
 
 
 def basket_contents(request):
@@ -10,26 +11,38 @@ def basket_contents(request):
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
+    basket_bin = []
 
     for product_id, quantity in basket.items():
         product = get_object_or_404(Vinyl, pk=product_id)
-        product_name = product.title.replace(" ", "_").lower
-        total += Decimal(quantity) * product.price
-        item_total = Decimal(quantity) * product.price
-        product_count += int(quantity)
-        product_images = product.image_set.all()
-        stock_quantity_list = []
-        for value in range(1, (product.stock_quantity + 1)):
-            stock_quantity_list.append(value)
-        basket_products.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-            'product_images': product_images,
-            'item_total': item_total,
-            'product_name': product_name,
-            'stock_quantity_list': stock_quantity_list
-        })
+        if quantity <= product.stock_quantity:
+            product_name = product.title.replace(" ", "_").lower
+            total += Decimal(quantity) * product.price
+            item_total = Decimal(quantity) * product.price
+            product_count += int(quantity)
+            product_images = product.image_set.all()
+            stock_quantity_list = []
+            for value in range(1, (product.stock_quantity + 1)):
+                stock_quantity_list.append(value)
+            basket_products.append({
+                'product_id': product_id,
+                'quantity': quantity,
+                'product': product,
+                'product_images': product_images,
+                'item_total': item_total,
+                'product_name': product_name,
+                'stock_quantity_list': stock_quantity_list
+            })
+        else:
+            messages.error(request, (
+                f'Sorry { product.title} no longer has { quantity }'
+                'left in stock.'))
+            basket_bin.append(product_id)
+
+    if basket_bin:
+        for product_id in basket_bin:
+            basket.pop(product_id)
+            request.session['basket'] = basket
 
     delivery = 4.95 + ((product_count - 1)/2)
     grand_total = round((total + Decimal(delivery)), 2)
