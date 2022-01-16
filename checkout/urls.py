3cdfc21eef1b0cb24b15f356cb25e6bc88b9ff8d
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-16 18:13:42 +0000

    Add webhook import and url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 """ Imports required basket app urls """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout_success/<order_number>',
          views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
