e1a1bb0674d0b3a03d94c8ccd902f5ee7c928279
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-16 23:55:57 +0000

    Add cache_checkout_data

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 import json
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 import stripe
@@ -8,6 +9,21 @@ from products.models import Vinyl
 from .forms import OrderForm, DeliveryForm
 from .models import Order, OrderLineItem
 
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'basket': json.dumps(request.session.get('basket', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 def checkout(request):
 
