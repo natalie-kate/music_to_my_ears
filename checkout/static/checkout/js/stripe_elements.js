e15f1a6daa279745e8984bb6aa2f9e92c7aae520
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-17 21:34:56 +0000

    Fix syntax difference, add emailinto billing

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -44,8 +44,7 @@ form.addEventListener('submit', function(ev) {
     $('#payment-form').hide();
     $('#loading-overlay').show();
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
+    var saveInfo = Boolean($('#save-info').attr('checked'));
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
@@ -54,14 +53,15 @@ form.addEventListener('submit', function(ev) {
     };
     var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function() {
+    $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
                 billing_details: {
                     name: $.trim(form.first_name.value) + $.trim(form.surname.value),
                     phone: $.trim(form.phone_number.value),
-                    address: {
+                    email: $.trim(form.email.value),
+                    address:{
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
@@ -74,7 +74,7 @@ form.addEventListener('submit', function(ev) {
             shipping: {
                 name: $.trim(form.first_name.value) + $.trim(form.surname.value),
                 phone: $.trim(form.phone_number.value),
-                address: {
+                address:{
                     line1: $.trim(form.street_address1.value),
                     line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town_or_city.value),
@@ -82,7 +82,7 @@ form.addEventListener('submit', function(ev) {
                     postal_code: $.trim(form.postcode.value),
                     state: $.trim(form.county.value),
                 }
-            }
+            },
         }).then(function(result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
@@ -102,7 +102,7 @@ form.addEventListener('submit', function(ev) {
                 }
             }
         });
-    }).fail(function() {
+    }) .fail(function() {
         // just reload the page, the error will be in django messages
         location.reload();
     })
