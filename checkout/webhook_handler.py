c9d152c47dd23926950f8fbec45a684046470f61
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-18 00:20:33 +0000

    Add variables to store wh data and check if order exists in db

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,9 +15,34 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """Handle the payment_intent.payment_succeeded webhook from Stripe """
+        intent = event.data.object
+        pid = intent.id
+        bag = intent.metadata.bag
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        order_exists = None
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(stripe_pid_iexact=pid)
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            self._send_confirmation_email(order)
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+
 
     def handle_payment_intent_payment_failed(self, event):
         """Handle the payment_intent.payment_failed webhook from Stripe """
