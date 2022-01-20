9574af1d0bbd1a703d192469987b56d3076353b9
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-20 20:05:08 +0000

    Add profile urls

--- music_to_my_ears/urls.py
+++ music_to_my_ears/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('shop/', include('products.urls')),
     path('basket/', include('basket.urls')),
-    path('checkout/', include('checkout.urls'))
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
