501ffca7eb39c1caba1779cb8f6829ff3b9eb38e
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-21 01:40:21 +0000

    Add context and  template to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile, SavedAddress
+from checkout.models import Order
+
 
 def profile(request):
-    return render(request, 'profiles/profile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    addresses = SavedAddress.objects.filter(user=profile)
+    orders = Order.objects.filter(user_profile=profile)
+    context = {
+        'profile': profile,
+        'addresses': addresses,
+        'orders': orders
+    }
+    template = 'profiles/profile.html'
+    return render(request, template, context)
