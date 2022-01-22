ef4d90619accd5db0022a7687c70e8f1ce2d6401
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-22 22:55:08 +0000

    Add order_history view

--- profiles/views.py
+++ profiles/views.py
@@ -50,3 +50,13 @@ def profile(request):
     return render(request, template, context)
 
 
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
