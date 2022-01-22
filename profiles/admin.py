4897e25dc335724555d83b8c9ba0e02268ad2651
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-22 17:53:47 +0000

    Add SavedAddressAdmin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SavedAddress
 
-# Register your models here.
+class SavedAddressAdmin(admin.ModelAdmin):
+    """Change display in admin panel to see important information """
+    list_display = (
+        'user',
+        'saved_street_address1',
+    )
+    ordering = ('user',)
+
+admin.site.register(SavedAddress, SavedAddressAdmin)
