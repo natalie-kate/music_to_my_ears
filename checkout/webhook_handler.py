5330651358e54d901399cc1ac3935d8f00611aeb
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-23 23:11:32 +0000

    Add in save_info code from views.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,6 +3,8 @@ import json
 from django.http import HttpResponse
 from products.models import Vinyl
 from .models import Order, OrderLineItem
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm, SavedAddressForm
 
 
 class StripeWH_Handler:
@@ -25,6 +27,72 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         delivery_details = intent.shipping
+        
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile_data = {
+                    'default_first_name': billing_details.name.split(" ")[0],
+                    'default_surname': billing_details.name.split(" ")[1],
+                    'default_email': billing_details.email,
+                    'default_phone_number': billing_details.phone,
+                    'default_street_address1': billing_details.address.line1,
+                    'default_street_address2': billing_details.address.line2,
+                    'default_town_or_city': billing_details.address.city,
+                    'default_county': billing_details.address.state,
+                    'default_country': billing_details.address.country,
+                    'default_postcode': billing_details.address.postal_code,
+                }
+                save_address_data = {
+                    'saved_street_address1': delivery_details.address.line1,
+                    'saved_street_address2': delivery_details.address.line2,
+                    'saved_town_or_city': delivery_details.address.city,
+                    'saved_county': delivery_details.address.state,
+                    'saved_country': delivery_details.address.country,
+                    'saved_postcode': delivery_details.address.postal_code,
+                }
+                user_profile_form = UserProfileForm(profile_data, instance=profile)
+                
+                if user_profile_form.is_valid():
+                    user_profile_form.save()
+                    try:
+                        address = SavedAddress.objects.get(
+                            saved_street_address1__iexact=order.street_address1,
+                            user=request.user,
+                        )
+                        save_address = True
+                    except SavedAddress.DoesNotExist:
+                        save_profile_address = {
+                            'saved_street_address1': billing_details.address.line1,
+                            'saved_street_address2': billing_details.address.line2,
+                            'saved_town_or_city': billing_details.address.city,
+                            'saved_county': billing_details.address.state,
+                            'saved_country': billing_details.address.country,
+                            'saved_postcode': billing_details.address.postal_code,
+                        }
+                        save_address_form = SavedAddressForm(save_profile_address)
+                        save_address = False
+                    if not save_address:
+                        address = save_address_form.save(commit=False)
+                        address.user = request.user
+                        address.save()
+
+            save_address_form = SavedAddressForm(save_address_data)
+            if save_address_form.is_valid():
+                try:
+                    address = SavedAddress.objects.get(
+                        saved_street_address1__iexact=order.delivery_street_address1,
+                        user=request.user,
+                    )
+                    save_address = True
+                except SavedAddress.DoesNotExist:
+                    save_address = False
+                if not save_address:
+                    address = save_address_form.save(commit=False)
+                    address.user = request.user
+                    address.save()
 
         order_exists = None
         attempt = 1
@@ -45,6 +113,7 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
+                    user_profile=profile,
                     first_name=billing_details.name.split(" ")[0],
                     surname=billing_details.name.split(" ")[1],
                     email=billing_details.email,
