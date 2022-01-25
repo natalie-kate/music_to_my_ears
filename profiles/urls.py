27bdf8b8f2970c9f66cf9220159a8ec5fa7422c6
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-25 03:33:57 +0000

    Add delete user url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('delete_user/<user_id>/', views.delete_user, name='delete_user'),
 ]
