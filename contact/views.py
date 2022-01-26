7166a8c7adf7bef2823eb66d6cb8fdd76256f8b2
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-26 03:44:50 +0000

    Start contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def contact(request):
+    """ A view to return the contact page """
+    return render(request, 'contact/contact.html')
