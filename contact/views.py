41f7285510f486be66c79b49375b5a208de5e90b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-26 23:30:18 +0000

    Send in user information to prefill form if authenticated

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from profiles.models import UserProfile
 
 def contact(request):
     """ A view to return the contact page """
-    return render(request, 'contact/contact.html')
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            contact_form = ContactForm(initial={
+                'first_name': profile.default_first_name,
+                'surname': profile.default_surname,
+                'email': profile.default_email,
+            })
+        except UserProfile.DoesNotExist:
+            contact_form = ContactForm()
+    else:
+        contact_form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'contact_form': contact_form
+    }
+    return render(request, template, context)
