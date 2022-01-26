d267de4ccefc9b60832a517a7b441d57e7c5e9bf
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-26 01:06:26 +0000

    Add view all path

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('shop/', views.open_shop, name='shop'),
+    path('all/', views.view_all_products, name='view_all_products'),
     path('<int:product_id>/', views.product, name='product'),
     path('add/', views.add_vinyl, name='add_vinyl'),
     path('edit/<int:product_id>/', views.edit_vinyl, name='edit_vinyl'),
