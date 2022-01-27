3f03105c0adf76bb50eb59af6cf7fecc73a8af3b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-27 00:22:06 +0000

    Register ContactAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('email_date', 'query', 'order_number', 'user', 'rate_us')
+    ordering = ('-email_date', 'user',)
+
+
+admin.site.register(Contact, ContactAdmin)
