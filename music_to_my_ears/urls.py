90d6a0409cb54c159f51189184062995c9f9c9e5
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-28 01:25:21 +0000

    Add events urls

--- music_to_my_ears/urls.py
+++ music_to_my_ears/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+    path('events/', include('events.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
