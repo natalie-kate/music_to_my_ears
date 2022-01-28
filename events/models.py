0ba10a136998070c282d6ad8badc7466fae39a36
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-28 01:07:17 +0000

    Create Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    """ Creates Event table in database """
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL,
+        null=True, blank=True)
+    name = models.CharField(max_length=254, null=False, blank=False)
+    date = models.CharField(max_length=254, null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
+    ticket_price = models.DecimalField(max_digits=6, decimal_places=2)
+    details = models.TextField(null=False, blank=False)
+    location = models.TextField(null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
