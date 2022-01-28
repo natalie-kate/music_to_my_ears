0561a3c453329c1225b85ec56396b3fc6193bf1e
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-28 01:14:12 +0000

    Register EventAdmin class

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,16 @@
+# Register Event so can view in admin panel
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    """Change display in admin panel to see important information """
+    list_display = (
+        'name',
+        'date',
+        'location',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Event, EventAdmin)
