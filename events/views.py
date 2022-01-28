f65871c44a5911d775aaeeefaf7a4c3ec00b7412
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-28 01:19:24 +0000

    Start events views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import EventForm
+from .models import Event
 
-# Create your views here.
+def view_events(request):
+    events = Event.objects.all()
+    
+    context = {
+        'events': events,
+        'in_events': True,
+    }
+    return render(request, 'events/events.html', context)
+
+def add_event(request):
+    form = EventForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'events/add_event.html')
+
+def edit_event(request, event_id):
+    return render(request, 'events/edit_event.html')
+
+def delete_event(request, event_id):
+    return render(request, 'events/delete_event.html')
