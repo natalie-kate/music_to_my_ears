b3cab9ccfe9354401b6998796be2a95fef04b13e
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-30 02:31:34 +0000

    Fixed errors

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -33,7 +33,8 @@ def webhook(request):
 
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': (
+            handler.handle_payment_intent_payment_failed),
     }
 
     event_type = event['type']
