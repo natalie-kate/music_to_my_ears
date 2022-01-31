5ce5fef31452db6ea3ee8044989d803476899f55
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-01-31 15:24:11 +0000

    Add name to admin url

--- music_to_my_ears/urls.py
+++ music_to_my_ears/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('shop/', include('products.urls')),
