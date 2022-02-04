b9a8e17c74bfa1a224287d7d006de1ef067819bd
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-04 22:51:15 +0000

    Remove xframes options

--- music_to_my_ears/settings.py
+++ music_to_my_ears/settings.py
@@ -220,5 +220,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-X_FRAME_OPTIONS = 'ALLOW-FROM https://music-to-my-ears-nka.herokuapp.com/'
