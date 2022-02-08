c6609e56f6cd46d11f60bf06fd431b1296406e07
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-08 22:32:55 +0000

    Add error handling views

---
+++ music_to_my_ears/views.py
@@ -0,0 +1,12 @@
+""" Imports and views required for home app."""
+from django.shortcuts import render
+
+
+def error_404(request, exception):
+    """ Render 404.html template """
+    return render(request, '404.html', status=404)
+
+
+def error_500(request, exception):
+    """ Render 500.html template """
+    return render(request, '500.html', status=500)
