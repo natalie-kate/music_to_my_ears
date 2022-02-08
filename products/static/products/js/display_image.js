d19743fe6b4e4e8fc898d944451a1910babd06d2
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-08 21:59:37 +0000

    Add comments

--- products/static/products/js/display_image.js
+++ products/static/products/js/display_image.js
@@ -1,18 +1,24 @@
+// Declare variables
 const chooseDefault = document.getElementById("default-image");
 const imgPreview = document.getElementById("default-img-preview");
 const chooseImages = document.getElementById("additional-images");
 const otherPreview = document.getElementById("img-preview");
 
+// If there is a chooseDefault input add change event listener and
+// send input and div thats to display the images to getImgData function
 if (chooseDefault) {
     chooseDefault.addEventListener("change", function () {
         getImgData(imgPreview, chooseDefault);
     });
 }
 
+// Add change event listener to other image input field and send
+// input and div thats to display the images to getImgData function
 chooseImages.addEventListener("change", function () {
     getImgData(otherPreview, chooseImages);
 });
 
+// Get each file that has been input and send to displayImg function
 function getImgData(img, imgFiles) {
     var files = imgFiles.files;
     if (files.length > 1) {
@@ -23,6 +29,7 @@ function getImgData(img, imgFiles) {
         displayImg(files)
     }
     
+    // Takes each individual image file an displays in div
     function displayImg(files) {
         if (files) {
             const fileReader = new FileReader();
