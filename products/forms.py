6a9d8c393f26832354b8fc50927a380fea8035ac
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-08 22:00:20 +0000

    Add docstrings, required and autofocus  attributes

--- products/forms.py
+++ products/forms.py
@@ -1,18 +1,26 @@
+""" Imports required for forms """
 from django import forms
 from .models import Vinyl, Genre
 from .validators import validate_tracklist
- 
+
 
 class ProductForm(forms.ModelForm):
-
+    """ Create ProductForm class """
     class Meta:
+        """ Fields to render from model """
         model = Vinyl
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
+        """ Add placeholders and required attribute,
+        set autofocus on first field """
         super().__init__(*args, **kwargs)
         genres = Genre.objects.all()
         friendly_names = [(g.id, g.get_friendly_name()) for g in genres]
+        for field in self.fields:
+            if self.fields[field].required:
+                self.fields[field].widget.attrs['required'] = True
+        self.fields['title'].widget.attrs['autofocus'] = True
         self.fields['description'].widget.attrs['placeholder'] = (
             'e.g 2nd studio album')
         self.fields['track_list'].widget.attrs['placeholder'] = (
