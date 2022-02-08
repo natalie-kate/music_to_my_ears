7cb5e11e17a77bbd2631bc9269ad52642508f4d8
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-08 22:04:10 +0000

    Remove js from shop and product.html and create new file

---
+++ products/static/products/js/carousel.js
@@ -0,0 +1,6 @@
+// Add active class to the first child of each carousel
+$(document).ready(function () {
+$(".carousel-inner" ).each(function( index ) {
+    $(this).children().first().addClass('active')
+})
+});
