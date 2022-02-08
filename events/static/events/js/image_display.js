034ff7568b6f873c885bb61dc6d55cf26c5e6c50
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-08 21:57:10 +0000

    Add comments

--- events/static/events/js/image_display.js
+++ events/static/events/js/image_display.js
@@ -1,11 +1,15 @@
+// Declare variables
 const chooseDefault = document.getElementById("id_image");
 const imgPreview = document.getElementById("img-preview");
 
+// Add change event listener to file input field
 chooseDefault.addEventListener("change", function () {
     var files = chooseDefault.files[0]
     if (files) {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(files);
+        // Add load event listener to file reader to display
+        // chosen image file in img preview div
         fileReader.addEventListener("load", function () {
             imgPreview.style.display = "block";
             imgPreview.innerHTML += '<img src="' + this.result + '" />';
