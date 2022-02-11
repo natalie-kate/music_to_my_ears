e5bdb678d54b41c0ad194ed55083f0fdf3bdc18b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-11 10:25:59 +0000

    Create seperate js rather than having at bottom of template

---
+++ contact/static/events/js/rating.js
@@ -0,0 +1,9 @@
+// Add checked attribute to first rating
+// Change ratings labels to vinyls
+$(document).ready(function () {
+    $("#id_rate_us_1").attr('checked', 'checked');
+    $(".form-check-label" ).each(function( index ) {
+      let html =("<i class='fas fa-record-vinyl'></i>")
+      $(this).html(html.repeat(index))
+    })
+});
