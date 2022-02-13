700e91dd2af2127df38f805fa4684213544fa374
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-13 16:42:14 +0000

    Add aria-hidden=true to icons

--- contact/static/contact/js/rating.js
+++ contact/static/contact/js/rating.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     $("#id_rate_us_1").attr('checked', 'checked');
     $(".form-check-label" ).each(function( index ) {
-      let html =("<i class='fas fa-record-vinyl'></i>")
+      let html =("<i class='fas fa-record-vinyl' aria-hidden='true'></i>")
       $(this).html(html.repeat(index))
     })
 });
