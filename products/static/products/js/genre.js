b0a8b991e836b3799655f90515b3ac681161f62b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-15 23:23:27 +0000

    Edit value to id

--- products/static/products/js/genre.js
+++ products/static/products/js/genre.js
@@ -1,7 +1,6 @@
 // Make Choose genre selected and disabled
 $(document).ready(function () {
-    option = document.querySelector("[value='choose_genre']");
-    console.log(option)
+    option = document.querySelector("[value='12']");
     $(option).attr("selected", "true")
     $(option).attr("disabled", "disabled")
 });
