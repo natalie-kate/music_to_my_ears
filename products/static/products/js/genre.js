3a7a8f134a1451f3b8d87218ce9eb327b237d1f9
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-17 00:20:40 +0000

    Try different way of adding default option.

--- products/static/products/js/genre.js
+++ products/static/products/js/genre.js
@@ -1,6 +1,6 @@
 // Make Choose genre selected and disabled
 $(document).ready(function () {
-    var option = document.querySelector("[value='14']");
-    $(option).attr("selected", "true");
-    $(option).attr("disabled", "disabled");
+    var option = document.querySelector("[value='1']");
+    $(option).before(
+        "<option selected='true' value='0' disabled='disabled' placeholder='Choose Genre'>Choose Genre</option>");
 });
