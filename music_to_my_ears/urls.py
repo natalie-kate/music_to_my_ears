86b9ce3471e60a928441c81e826513f5b1af3655
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-17 22:43:57 +0000

    Remove handlers

--- music_to_my_ears/urls.py
+++ music_to_my_ears/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
@@ -32,5 +31,3 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = "music_to_my_ears.views.error_404"
-handler500 = "music_to_my_ears.views.error_500"
