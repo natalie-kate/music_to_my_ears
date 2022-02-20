29340400de5915b3d9a32b05d722a80d87550c7a
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-20 22:27:26 +0000

    Add to TestViews

--- products/test_views.py
+++ products/test_views.py
@@ -1,5 +1,6 @@
 """ Import TestCase Vinyl and Genre to test our views """
 from django.test import TestCase
+from django.urls import reverse
 from .models import Vinyl, Genre
 
 
@@ -12,6 +13,7 @@ class TestViews(TestCase):
                 genre="test",
                 friendly_name="Test"
             )
+
         self.vinyl = Vinyl.objects.create(
             title="Test",
             artist="bla",
@@ -25,12 +27,22 @@ class TestViews(TestCase):
 
     def test_shop_view(self):
         """ Test shop.html rendered properly """
-        response = self.client.get('/shop/')
+        response = self.client.get('/shop/shop/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/shop.html')
 
+    def test_shop_url_accessible_by_name(self):
+        """ Test shop link """
+        response = self.client.get(reverse('shop'))
+        self.assertEqual(response.status_code, 200)
+
     def test_product_view(self):
         """ Test product.html rendered properly """
-        response = self.client.get(f'/{ self.vinyl.id }/')
+        response = self.client.get(f'/shop/{ self.vinyl.id }/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product.html')
+
+    def test_product_url_accessible_by_name(self):
+        """ Test shop link """
+        response = self.client.get(reverse('product', args=[self.vinyl.id]))
+        self.assertEqual(response.status_code, 200)
