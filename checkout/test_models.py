4232748bc4e53e4080aff259d6f2e054f520c8c9
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-02-25 18:07:09 +0000

    Start set up for testcheckout models

--- checkout/test_models.py
+++ checkout/test_models.py
@@ -1,3 +1,71 @@
+""" Imports to test our checkout models """
 from django.test import TestCase
+from .models import Order, OrderLineItem
+from products.models import Genre, Vinyl, Image
 
-# Create your tests here.
+
+class TestOrderModels(TestCase):
+    """ Test order models """
+    def setUp(self):
+        """ Create test records to use """
+        self.genre = Genre.objects.create(
+            genre="test_genre",
+            friendly_name="Test Genre"
+        )
+
+        self.product = Vinyl.objects.create(
+            genre=self.genre,
+            title=
+            artist=
+            price=
+            stock_quantity=
+            track_list=
+        )
+
+        self.product1 = Vinyl.objects.create(
+            
+        )
+
+        self.product2 = Vinyl.objects.create(
+            
+        )
+
+        self.image = Image.objects.create(
+            default=True,
+            vinyl=self.vinyl,
+            image_name="image.png"
+        )
+
+        self.order = Order.objects.create(
+            order_number =
+            user_profile = 
+            first_name = 
+            surname =
+            email = 
+            phone_number = 
+            street_address1 = 
+            town_or_city = 
+            country = 
+            )
+
+        self.lineitem = OrderLineItem.create(
+            order = 
+            product = 
+            quantity = 
+            lineitem_total = 
+        )
+
+        self.lineitem1 = OrderLineItem.create(
+            order = 
+            product = 
+            quantity = 
+            lineitem_total = 
+        )
+
+        self.lineitem2 = OrderLineItem.create(
+            order = 
+            product = 
+            quantity = 
+            lineitem_total = 
+        )
+
